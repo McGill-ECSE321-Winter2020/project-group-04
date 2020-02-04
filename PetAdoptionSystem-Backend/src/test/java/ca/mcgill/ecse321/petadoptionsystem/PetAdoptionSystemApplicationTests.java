f0ed60685c088f975215c0fc0dc468d84c9228a3
Ousmane BARICISSE <ousmane.baricisse@mail.mcgill.ca>
2020-02-03 22:59:57 -0500

    Initial commit of the backend application

---
+++ PetAdoptionSystem-Backend/src/test/java/ca/mcgill/ecse321/petadoptionsystem/PetAdoptionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.petadoptionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAdoptionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
