38798aa6eb28eaa0ded97fc811ad01d732c59f6a
tangj0 <jtang186@gmail.com>
2020-02-04 21:25:20 -0500

    modify test file

--- PetAdoptionSystem-Backend/src/test/java/ca/mcgill/ecse321/petadoptionsystem/PetAdoptionSystemApplicationTests.java
+++ PetAdoptionSystem-Backend/src/test/java/ca/mcgill/ecse321/petadoptionsystem/PetAdoptionSystemApplicationTests.java
@@ -1,13 +1,20 @@
 package ca.mcgill.ecse321.petadoptionsystem;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@SpringBootTest
-class PetAdoptionSystemApplicationTests {
+@RestController
+@SpringBootApplication
+public class PetAdoptionSystemTests {
+       	public static void main(String[] args) {
+		SpringApplication.run(PetAdoptionSystemTests.class, args);
+       	}
 
-	@Test
-	void contextLoads() {
+	@RequestMapping("/")
+	public String greeting(){
+		return "Hello world!";
 	}
-
+	    
 }
