621193cdc0138f10fefb31b203dc6b2917e9659f
Ousmane BARICISSE <ousmane.baricisse@mail.mcgill.ca>
2020-02-08 18:20:36 -0500

    Update PetAdoptionSystemApplicationTests.java

--- PetAdoptionSystem-Backend/src/test/java/ca/mcgill/ecse321/petadoptionsystem/PetAdoptionSystemApplicationTests.java
+++ PetAdoptionSystem-Backend/src/test/java/ca/mcgill/ecse321/petadoptionsystem/PetAdoptionSystemApplicationTests.java
@@ -1,17 +1,27 @@
 package ca.mcgill.ecse321.petadoptionsystem;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.RestController;
+
+import ca.mcgill.ecse321.petadoptionsystem.model.PetAdoptionSystem;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class PetAdoptionSystemApplicationTests {
+	
+	@Autowired
+	private PetAdoptionSystem petadoptionsystem;
 	@Test
 	void contextLoad(){
-		
+
 	}
 }
