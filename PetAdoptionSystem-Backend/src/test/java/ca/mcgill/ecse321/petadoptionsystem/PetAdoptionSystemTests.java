d18f2282b63101d0fe9bd71656e512745df485dc
Edem Nuviadenu <edemnuviadenu@gmail.com>
2020-02-09 02:57:14 -0500

    Revert "fix applicationContest error + pass imageTest"
    This reverts commit 2f1b9b976b96eb065d00898d2b91d7013e5e893d.

--- PetAdoptionSystem-Backend/src/test/java/ca/mcgill/ecse321/petadoptionsystem/PetAdoptionSystemTests.java
+++ PetAdoptionSystem-Backend/src/test/java/ca/mcgill/ecse321/petadoptionsystem/PetAdoptionSystemTests.java
@@ -16,7 +16,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 public class PetAdoptionSystemTests {
     @Autowired
