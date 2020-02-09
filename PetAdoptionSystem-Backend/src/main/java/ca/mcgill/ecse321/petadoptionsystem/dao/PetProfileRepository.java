6aed237e9b07d2f092fca66399016cd6d955645d
Ousmane BARICISSE <ousmane.baricisse@mail.mcgill.ca>
2020-02-09 13:54:35 -0500

    update repository

--- PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/dao/PetProfileRepository.java
+++ PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/dao/PetProfileRepository.java
@@ -5,15 +5,9 @@ import org.springframework.stereotype.Repository;
 
 import ca.mcgill.ecse321.petadoptionsystem.model.PetProfile;
 
-
+@Repository
 public interface PetProfileRepository extends CrudRepository<PetProfile, Integer> {
 
-
-    PetProfile findPetProfileByName(String name);
-
-	//PetProfile findPetProfileByUsername(int id);
-
 	PetProfile findPetProfileById(int i);
-
 
 }
