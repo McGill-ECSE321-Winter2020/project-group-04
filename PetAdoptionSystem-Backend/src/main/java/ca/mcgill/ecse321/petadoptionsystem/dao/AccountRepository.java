56463722c577e8907e17bdf7a0ede4f00a6be949
tangj0 <jtang186@gmail.com>
2020-02-09 09:44:58 -0500

    gradle build successful, solved by deleting child class first

--- PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/dao/AccountRepository.java
+++ PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/dao/AccountRepository.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse321.petadoptionsystem.dao;
 
+import ca.mcgill.ecse321.petadoptionsystem.model.PetAdoptionSystem;
 import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.petadoptionsystem.model.Account;
@@ -7,6 +8,7 @@ import ca.mcgill.ecse321.petadoptionsystem.model.Account;
 
 public interface AccountRepository extends CrudRepository<Account, Integer> {
 
-	
+	Account findAccountByUsername(String username);
+	//Account findAccountByUsernameAndPetAdoptionSystem(String username, PetAdoptionSystem system);
 
 }
--- PetAdoptionSystem-Backend/src/test/java/ca/mcgill/ecse321/petadoptionsystem/AccountTest.java
+++ PetAdoptionSystem-Backend/src/test/java/ca/mcgill/ecse321/petadoptionsystem/AccountTest.java
@@ -1,7 +1,10 @@
 package ca.mcgill.ecse321.petadoptionsystem;
 
 import ca.mcgill.ecse321.petadoptionsystem.dao.AccountRepository;
+import ca.mcgill.ecse321.petadoptionsystem.dao.PetAdoptionSystemRepository;
+import ca.mcgill.ecse321.petadoptionsystem.dao.RegularUserRepository;
 import ca.mcgill.ecse321.petadoptionsystem.model.Account;
+import ca.mcgill.ecse321.petadoptionsystem.model.PetAdoptionSystem;
 import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,30 +17,43 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class AccountTest {
-//    @Autowired
-//    private AccountRepository accountRepository;
-//
-//    @AfterEach
-//    public void clearDataBase(){
-//        accountRepository.deleteAll();
-//    }
-//
-//    @Test
-//    public void testPersistAndLoadRegistration(){
-//
-//        Account account = new Account();
-//        String userName = "JohnDoe361";
-//        String email = "johndoe1955@gmail.com";
-//        //int idea = 1111;
-//        //String passwordHash = "868345C2BF61B5BA913D578B674838CB08A3575B"; //greenFurryPuppies123, SHA1
-//
-//        account.setUsername(userName);
-//        account.setEmail(email);
-//        //account.setPetAdoptionSystem();
-//        //account.setPasswordHash(passwordHash);
-//        accountRepository.save(account);
-//    }
-//
+    @Autowired
+    private PetAdoptionSystemRepository petAdoptionSystemRepository;
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @AfterEach
+    public void clearDataBase(){
+        accountRepository.deleteAll();
+        petAdoptionSystemRepository.deleteAll();
+    }
+
+    @Test
+    public void testPersistAndLoadAccount(){
+        int sysId = 123;
+        PetAdoptionSystem system = TestingUtility.initPetAdoptionSystem(sysId);
+        petAdoptionSystemRepository.save(system);
+
+        String username = "JohnDoe361";
+        String email = "johndoe1955@gmail.com";
+        Account account = TestingUtility.initAccount(username, email, system);
+        //String passwordHash = "868345C2BF61B5BA913D578B674838CB08A3575B"; //greenFurryPuppies123, SHA1
+
+        //account.setUsername(username);
+        //account.setEmail(email);
+        //account.setPetAdoptionSystem();
+        //account.setPasswordHash(passwordHash);
+        accountRepository.save(account);
+
+//        system = null;
+//        system = petAdoptionSystemRepository.findPetAdoptionSystemById(sysId);
+//        account = null;
+//        account = accountRepository.findAccountByUsernameAndPetAdoptionSystem(username, system);
+        account = null;
+        account = accountRepository.findAccountByUsername(username);
+
+    }
+
 //    @Test
 //    public void testLoadPetAdoptionSystemId(){
 //        Account account = new Account();
