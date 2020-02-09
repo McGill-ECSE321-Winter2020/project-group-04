195f733f2c65f9955ee6094b2b8961c84379266a
Ousmane BARICISSE <ousmane.baricisse@mail.mcgill.ca>
2020-02-08 23:47:03 -0500

    Create TestingUtility.java

---
+++ PetAdoptionSystem-Backend/src/test/java/ca/mcgill/ecse321/petadoptionsystem/TestingUtility.java
@@ -0,0 +1,85 @@
+package ca.mcgill.ecse321.petadoptionsystem;
+
+import ca.mcgill.ecse321.petadoptionsystem.model.*;
+
+class TestingUtility {
+
+    public static PetAdoptionSystem initPetAdoptionSystem(int id){
+        PetAdoptionSystem pas = new PetAdoptionSystem();
+        pas.setId(id);
+
+        return pas;
+    }
+
+    public static Account initAccount(String username, String email, PetAdoptionSystem pas){
+        Account act = new Account();
+        
+        act.setUsername(username);
+        act.setEmail(email);
+        act.setPetAdoptionSystem(pas);
+        
+        return act;
+    }
+   
+    /**
+     * 
+     * @param pkey
+     * @param username
+     * @param petAdoptionSystemID
+     * @return
+     */
+
+    public static Admin initAdmin(int pkey, Account act, PetAdoptionSystem pas){
+        Admin admin = new Admin();
+        admin.setUser(act);
+        admin.setId(pkey);
+        return admin;
+    }
+
+    public static RegularUser initRegularUser(int pkey, Account act, PetAdoptionSystem pas){
+        RegularUser user = new RegularUser();
+        user.setUser(act);
+        user.setId(pkey);
+        return user;
+    }
+
+    public static PetProfile initPetProfile(int pkey, RegularUser regUser, PetAdoptionSystem pas){
+       
+        PetProfile petProf = new PetProfile();
+        petProf.setId(pkey);
+        petProf.setPoster(regUser);
+
+        return petProf;
+    }
+
+   
+    public static AdoptionApplication initAdoptionApplication(int pkey, RegularUser regUser, PetProfile petProf){
+        AdoptionApplication ada = new AdoptionApplication();
+       
+        ada.setApplicant(regUser);
+        ada.setId(pkey);
+        ada.setPetProfile(petProf);
+        return ada;
+
+    }
+
+    public static Donation initDonation(int pkey, RegularUser regUser) {
+        
+        Donation don = new Donation();
+        don.setId(pkey);
+        don.setUser(regUser);
+
+        return don;
+
+    }
+
+    public static Image initImage(int pkey, PetProfile petProf){
+        Image img = new Image();
+        img.setId(pkey);
+        img.setPetProfile(petProf);
+
+        return img;
+
+    }
+    
+}
