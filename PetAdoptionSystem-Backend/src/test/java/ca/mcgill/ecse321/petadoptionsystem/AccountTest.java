b0b52ecc6ba45b77269caa09b829ac78f9658b5b
tangj0 <jtang186@gmail.com>
2020-02-09 10:01:35 -0500

    complete Account tests

--- PetAdoptionSystem-Backend/src/test/java/ca/mcgill/ecse321/petadoptionsystem/AccountTest.java
+++ PetAdoptionSystem-Backend/src/test/java/ca/mcgill/ecse321/petadoptionsystem/AccountTest.java
@@ -2,18 +2,16 @@ package ca.mcgill.ecse321.petadoptionsystem;
 
 import ca.mcgill.ecse321.petadoptionsystem.dao.AccountRepository;
 import ca.mcgill.ecse321.petadoptionsystem.dao.PetAdoptionSystemRepository;
-import ca.mcgill.ecse321.petadoptionsystem.dao.RegularUserRepository;
 import ca.mcgill.ecse321.petadoptionsystem.model.Account;
 import ca.mcgill.ecse321.petadoptionsystem.model.PetAdoptionSystem;
 import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.SpringApplication;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 public class AccountTest {
@@ -37,28 +35,17 @@ public class AccountTest {
         String username = "JohnDoe361";
         String email = "johndoe1955@gmail.com";
         Account account = TestingUtility.initAccount(username, email, system);
-        //String passwordHash = "868345C2BF61B5BA913D578B674838CB08A3575B"; //greenFurryPuppies123, SHA1
 
-        //account.setUsername(username);
-        //account.setEmail(email);
-        //account.setPetAdoptionSystem();
-        //account.setPasswordHash(passwordHash);
         accountRepository.save(account);
 
-//        system = null;
-//        system = petAdoptionSystemRepository.findPetAdoptionSystemById(sysId);
-//        account = null;
-//        account = accountRepository.findAccountByUsernameAndPetAdoptionSystem(username, system);
+        //test Account not null
+        assertNotNull(account);
+
+        //test Account data persistence
         account = null;
         account = accountRepository.findAccountByUsername(username);
+        assertEquals(email, account.getEmail());
 
     }
 
-//    @Test
-//    public void testLoadPetAdoptionSystemId(){
-//        Account account = new Account();
-//        account.getPetAdoptionSystem();
-//        accountRepository.save(account);
-//    }
-   
 }
