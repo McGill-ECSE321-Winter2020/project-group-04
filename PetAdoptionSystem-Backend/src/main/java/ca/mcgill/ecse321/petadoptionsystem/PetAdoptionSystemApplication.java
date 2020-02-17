8bfff8dc40059c985e5c4100048591f2981a7f7d
Joseantonio <ja.jijon.vorbeck@gmail.com>
2020-02-17 18:15:08 -0500

    Adding a line to test heroku

--- PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/PetAdoptionSystemApplication.java
+++ PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/PetAdoptionSystemApplication.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class PetAdoptionSystemApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(PetAdoptionSystemApplication.class, args);
 	}
