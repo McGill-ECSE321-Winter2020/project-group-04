c651d73dd511e92dbd92162580fb21b3c0ca8857
Joseantonio <ja.jijon.vorbeck@gmail.com>
2020-02-19 23:50:44 -0500

    Adding Service and DTO packages

---
+++ PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/Service/PetProfileService.java
@@ -0,0 +1,5 @@
+package ca.mcgill.ecse321.petadoptionsystem.Service;
+
+public class PetProfileService {
+
+}
---
+++ PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/dto/PetProfileDTO.java
@@ -0,0 +1,5 @@
+package ca.mcgill.ecse321.petadoptionsystem.dto;
+
+public class PetProfileDTO {
+
+}
