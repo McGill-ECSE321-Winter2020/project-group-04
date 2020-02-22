0c33da30d7abf522c5385e199229538d4394586b
Joseantonio <ja.jijon.vorbeck@gmail.com>
2020-02-22 17:17:06 -0500

    Changing packages names to lowercases

--- PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/Controller/PetProfileController.java
+++
@@ -1,5 +0,0 @@
-package ca.mcgill.ecse321.petadoptionsystem.Controller;
-
-public class PetProfileController {
-
-}
--- PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/Service/PetProfileService.java
+++
@@ -1,5 +0,0 @@
-package ca.mcgill.ecse321.petadoptionsystem.Service;
-
-public class PetProfileService {
-
-}
