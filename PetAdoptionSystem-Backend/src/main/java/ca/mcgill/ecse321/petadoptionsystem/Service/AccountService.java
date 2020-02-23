d83eb5f4eeddd35f1e2e0a35cd07b17927e8aa78
garrettkinman <garrettkinman@gmail.com>
2020-02-22 19:01:14 -0500

    added java and springboot imports; added stereotypes

--- PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/Service/AccountService.java
+++ PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/Service/AccountService.java
@@ -1,35 +1,53 @@
 package ca.mcgill.ecse321.petadoptionsystem.Service;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+// TODO dao and model imports
+
+@Service
 public class AccountService {
 
+
+    @Transactional
     public Account createRegularUserAccount(String username, String passwordHash, String email) {
         // TODO
     }
 
+    @Transactional
     public Account createAdminAccount(String username, String passwordHash, String email) {
         // TODO
     }
 
+    @Transactional
     public Account getAccount(String username) {
         // TODO
     }
 
+    @Transactional
     public Account getAccountByEmail(String email) {
         // TODO
     }
 
+    @Transactional
     public ArrayList<Account> getAllAccounts() {
         // TODO
     }
 
+    @Transactional
     public boolean updatePassword(String username, String newPasswordHash) {
         // TODO
     }
 
+    @Transactional
     public boolean updateEmail(String username, String newEmail) {
         // TODO
     }
 
+    @Transactional
     public boolean deleteAccount(String username) {
         // TODO
     }
