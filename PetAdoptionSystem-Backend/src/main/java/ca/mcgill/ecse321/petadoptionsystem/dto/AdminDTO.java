d9ef8a687797f6b850a19a34677e52ff7fbca4de
tangj0 <jtang186@gmail.com>
2020-02-23 18:03:36 -0500

    add admin DTO and controller classes #53

--- PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/dto/AdminDTO.java
+++ PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/dto/AdminDTO.java
@@ -1,4 +1,18 @@
 package ca.mcgill.ecse321.petadoptionsystem.dto;
 
+import ca.mcgill.ecse321.petadoptionsystem.model.Admin;
+
+import java.util.List;
+
 public class AdminDTO {
+    private AdminDTO admin;
+    //private List<AdminDTO> admins;
+    private int id;
+
+    /**
+     * @param id admin id
+     */
+    public AdminDTO(int id){
+        this.id = id;
+    }
 }
--- PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/service/AdminService.java
+++ PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/service/AdminService.java
@@ -3,12 +3,15 @@ package ca.mcgill.ecse321.petadoptionsystem.service;
 import ca.mcgill.ecse321.petadoptionsystem.dao.AdminRepository;
 import ca.mcgill.ecse321.petadoptionsystem.model.Admin;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hibernate.internal.util.collections.ArrayHelper.toList;
 
+@Service
 public class AdminService {
     @Autowired
     AdminRepository adminRepository;
@@ -18,7 +21,7 @@ public class AdminService {
      * @return Admin corresponding to input id
      */
     @Transactional
-    public Admin getAdmin(int id){
+    public Admin getAdminById(int id){
         Admin admin = null;
         try {
             admin = adminRepository.findAdminById(id);
@@ -33,14 +36,22 @@ public class AdminService {
      * @return list of all Admins currently in the Admin repository
      */
     @Transactional
-    public List<Admin> getAllAdmin() {
-        List admins = null;
-        try {
-            admins = toList(adminRepository.findAll());
-        }
-        catch(NullPointerException e){
-            System.out.println("NullPointerException: Empty Admin repository");
-        }
+    public List<Admin> getAllAdmins() {
+        List<Admin> admins;
+        admins = toList(adminRepository.findAll());
         return admins;
+    }
+
+    /**
+     * @param iterable
+     * @param <T>
+     * @return
+     */
+    private <T> List<T> toList(Iterable<T> iterable){
+        List<T> resultList = new ArrayList<T>();
+        for (T t : iterable) {
+            resultList.add(t);
+        }
+        return resultList;
     }
 }
