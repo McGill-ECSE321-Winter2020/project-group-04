7bdc0f825d6011c39da4f3d17544476e0b4cdf6d
garrettkinman <garrettkinman@gmail.com>
2020-02-24 00:12:05 -0500

    fixed two methods to throw exception #52

--- PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/Service/AccountService.java
+++ PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/Service/AccountService.java
@@ -20,56 +20,80 @@ public class AccountService {
     @Transactional
     public Account createRegularUserAccount(String username, String passwordHash, String email) {
         
-        // check if username, email, and passwordHash are valid
-        if (username != null && email != null && passwordHash != null) {
-            if (username.length() > 0 && email.length() > 0 && passwordHash.length() > 0) {
-                
-                // check if username and email are unique in the system
-                if (!(accountRepository.existsByUsername(username) || accountRepository.existsByEmail(email))) {
-                    Account account = new Account();
-                    account.setUsername(username);
-                    account.setPasswordHash(passwordHash);
-                    account.setEmail(email);
+        String error = "";
+
+        // check if valid inputs
+        if (username == null || username.length() == 0) error += "The username cannot be empty.\n";
+        if (passwordHash == null || passwordHash.length() == 0) error += "The password hash cannot be empty.\n";
+        if (email == null || email.length() == 0) error += "The email address cannot be empty.\n";
+
+        if (error.length() > 0) throw new IllegalArgumentException(error);
+
+
+        // check if username or email is taken
+        if (accountRepository.existsByUsername(username)) error += "That username is already taken.\n";
+        if (accountRepository.existsByEmail(email)) error += "That email address is already taken.\n";
+
+        if (error.length() > 0) throw new IllegalArgumentException(error);
+
+
+        // create account with the now-validated inputs
+        Account account = new Account();
+        account.setUsername(username);
+        account.setPasswordHash(passwordHash);
+        account.setEmail(email);
                     
-                    // TODO: create UserRole
+        // TODO: create UserRole
 
-                    accountRepository.save(account);
-                    return account;
-                }
-            }
-        }
-
-        return null;
+        accountRepository.save(account);
+        return account;
     }
 
     @Transactional
     public Account createAdminAccount(String username, String passwordHash, String email) {
         
-        // check if username, email, and passwordHash are valid
-        if (username != null && email != null && passwordHash != null) {
-            if (username.length() > 0 && email.length() > 0 && passwordHash.length() > 0) {
-                
-                // check if username and email are unique in the system
-                if (!(accountRepository.existsByUsername(username) || accountRepository.existsByEmail(email))) {
-                    Account account = new Account();
-                    account.setUsername(username);
-                    account.setPasswordHash(passwordHash);
-                    account.setEmail(email);
+        String error = "";
+
+        // check if valid inputs
+        if (username == null || username.length() == 0) error += "The username cannot be empty.\n";
+        if (passwordHash == null || passwordHash.length() == 0) error += "The password hash cannot be empty.\n";
+        if (email == null || email.length() == 0) error += "The email address cannot be empty.\n";
+
+        if (error.length() > 0) throw new IllegalArgumentException(error);
+
+
+        // check if username or email is taken
+        if (accountRepository.existsByUsername(username)) error += "That username is already taken.\n";
+        if (accountRepository.existsByEmail(email)) error += "That email address is already taken.\n";
+
+        if (error.length() > 0) throw new IllegalArgumentException(error);
+
+
+        // create account with the now-validated inputs
+        Account account = new Account();
+        account.setUsername(username);
+        account.setPasswordHash(passwordHash);
+        account.setEmail(email);
                     
-                    // TODO: create UserRole
+        // TODO: create UserRole
 
-                    accountRepository.save(account);
-                    return account;
-                }
-            }
-        }
-
-        return null;
+        accountRepository.save(account);
+        return account;
     }
 
     @Transactional
     public Account getAccount(String username) {
         // TODO
+
+        // check if username is valid
+        if (username != null) {
+            if (username.length() > 0) {
+
+            }
+        }
+
+        // if failed to get account
+        return null;
     }
 
     @Transactional
