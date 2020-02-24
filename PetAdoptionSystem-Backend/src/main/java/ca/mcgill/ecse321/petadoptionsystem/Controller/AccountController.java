ca979bd2652e414f6c57157f3df5e7c5f58dd489
garrettkinman <garrettkinman@gmail.com>
2020-02-24 17:52:01 -0500

    created accountcontroller #52

---
+++ PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/Controller/AccountController.java
@@ -0,0 +1,17 @@
+package ca.mcgill.ecse321.petadoptionsystem.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+import ca.mcgill.ecse321.petadoptionsystem.service.AccountService;
+
+@CrossOrigin(origins = "*")
+@RestController
+
+public class AccountController {
+
+    @Autowired
+    private AccountService accountService;
+    
+}
