cc8b3ef8f5aac854f5c4782920f83a311708d435
Ousmane BARICISSE <ousmane.baricisse@mail.mcgill.ca>
2020-02-28 10:20:09 -0500

    Image service and controller beta work #54  [ci skip]

---
+++ PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/dto/UploadImageDTO.java
@@ -0,0 +1,47 @@
+package ca.mcgill.ecse321.petadoptionsystem.dto;
+
+public class UploadImageDTO {
+    private String imageName;
+    private String imageDownloadUri;
+    private String imageType;
+    private long size;
+
+    public UploadImageDTO(String imageName, String imageDownloadUri, String imageType, long size) {
+        this.imageName = imageName;
+        this.imageDownloadUri = imageDownloadUri;
+        this.imageType = imageType;
+        this.size = size;
+    }
+
+    public String getimageName() {
+        return imageName;
+    }
+
+    public void setimageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public String getimageDownloadUri() {
+        return imageDownloadUri;
+    }
+
+    public void setimageDownloadUri(String imageDownloadUri) {
+        this.imageDownloadUri = imageDownloadUri;
+    }
+
+    public String getimageType() {
+        return imageType;
+    }
+
+    public void setimageType(String imageType) {
+        this.imageType = imageType;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+}
