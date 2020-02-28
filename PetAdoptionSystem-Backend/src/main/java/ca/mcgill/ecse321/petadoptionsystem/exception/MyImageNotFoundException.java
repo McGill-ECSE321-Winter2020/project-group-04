cf623bd394aed8fac6fa70e832ae2647ed49e657
Ousmane BARICISSE <ousmane.baricisse@mail.mcgill.ca>
2020-02-28 09:15:46 -0500

    update image model + imagenotfoundexception #54  [ci skip]

---
+++ PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/exception/MyImageNotFoundException.java
@@ -0,0 +1,7 @@
+package ca.mcgill.ecse321.petadoptionsystem.exception;
+
+public class MyImageNotFoundException extends RuntimeException {
+    public MyImageNotFoundException(String message){
+        super(message);
+    }
+}
--- PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/service/ImageService.java
+++ PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/service/ImageService.java
@@ -11,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import ca.mcgill.ecse321.petadoptionsystem.dao.ImageRepository;
 import ca.mcgill.ecse321.petadoptionsystem.dao.PetProfileRepository;
 import ca.mcgill.ecse321.petadoptionsystem.exception.FileStorageException;
+import ca.mcgill.ecse321.petadoptionsystem.exception.MyImageNotFoundException;
 import ca.mcgill.ecse321.petadoptionsystem.model.*;
 
 @Service
@@ -22,24 +23,40 @@ public class ImageService {
     private ImageRepository imageRepository;
 
     @Transactional
-    public void createImage(MultipartFile file, String Description){
+    public Image createImage(MultipartFile file, String Description, PetProfile petProfile){
         String imageName = StringUtils.cleanPath(file.getOriginalFilename());
         try{
             if(imageName==null || imageName.trim().length()==0){
                 throw new FileStorageException("Imagename contains invalid path sequence " + imageName);
+            }
+            if(petProfile==null){
+                throw new FileStorageException("Pet Profile associated to image is null");
             }
             //PetProfile petProfile = petProfileRepository.
             Image image = new Image();
             image.setData(file.getBytes());
             image.setImageName(imageName);
             image.setImageType(file.getContentType());
+            image.setDescription(Description);
+            image.setPetProfile(petProfile);
+            imageRepository.save(image);
             
-            
+            return image;
         } catch(IOException ex){
             String error = "Could not store file " + imageName + ". Please try again!";
             throw new FileStorageException(error, ex);
         }
 
     }
+
+    @Transactional
+    public Image getImage(String imageName){
+        Image img = imageRepository.findImageByImageName(imageName);
+        if(img==null) {
+            throw new MyImageNotFoundException("Image not found with id " + imageName);
+        }
+        return img;
+    }
+
 
 }
