821bd4c5b03f517e6efb1a89875c4451f0a0792d
Edem Nuviadenu <edemnuviadenu@gmail.com>
2020-02-27 23:59:58 -0500

    Revert "Merge branch 'master' into merge"
    This reverts commit 26074f975ebb44308c60ec1a79513df6521ecfe5, reversing
    changes made to 9db89e2580b40aad5df0741f8fd3dc5998974cf6.

--- PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/service/PetProfileService.java
+++ PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/service/PetProfileService.java
@@ -1,6 +1,5 @@
 package ca.mcgill.ecse321.petadoptionsystem.service;
 
-
 import ca.mcgill.ecse321.petadoptionsystem.dao.AccountRepository;
 import ca.mcgill.ecse321.petadoptionsystem.dao.PetProfileRepository;
 import ca.mcgill.ecse321.petadoptionsystem.dao.RegularUserRepository;
