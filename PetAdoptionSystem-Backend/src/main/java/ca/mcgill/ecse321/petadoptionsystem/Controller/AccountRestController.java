beb0a65be4a8757c54ff8c84cd1494f24a950684
garrettkinman <garrettkinman@gmail.com>
2020-02-29 15:07:15 -0500

    added getAccountByEmail and username controller methods #52

--- PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/Controller/AccountRestController.java
+++ PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/Controller/AccountRestController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.mcgill.ecse321.petadoptionsystem.service.AccountService;
@@ -19,7 +20,7 @@ public class AccountRestController {
     @Autowired
     private AccountService accountService;
     
-    @GetMapping(value = {"/accounts", "/accounts/"})
+    @GetMapping(value = {"/account/all", "/account/all/"})
     public List<AccountDTO> getAllAccounts() throws IllegalArgumentException {
         List<AccountDTO> accountDTOs = new ArrayList<AccountDTO>();
 
@@ -30,7 +31,15 @@ public class AccountRestController {
         return accountDTOs;
     }
 
-    // TODO: rest of the REST methods
+    @GetMapping(value = {"/account/username/{username}", "/account/username/{username}/"})
+    public AccountDTO getAccountByUsername(@PathVariable("username") String username) throws IllegalArgumentException {
+        return convertToDTO(accountService.getAccountByUsername(username));
+    }
+
+    @GetMapping(value = {"/account/email/{email}", "/account/email/{email}/"})
+    public AccountDTO getAccountsByEmail(@PathVariable("email") String email) throws IllegalArgumentException {
+        return convertToDTO(accountService.getAccountByEmail(email));
+    }
 
     private AccountDTO convertToDTO(Account account) {
         return null;
