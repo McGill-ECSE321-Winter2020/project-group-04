96a64e8ca5481ea3a28f8358ffeca8df0aa7d4bd
garrettkinman <garrettkinman@gmail.com>
2020-02-29 12:51:04 -0500

    set up the basics of rest and dto #52

--- PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/Controller/AccountRestController.java
+++ PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/Controller/AccountRestController.java
@@ -5,13 +5,23 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.mcgill.ecse321.petadoptionsystem.service.AccountService;
+import ca.mcgill.ecse321.petadoptionsystem.dto.AccountDTO;
 
 @CrossOrigin(origins = "*")
 @RestController
-
-public class AccountController {
+public class AccountRestController {
 
     @Autowired
     private AccountService accountService;
     
+    @GetMapping(value = {"/accounts", "/accounts/"})
+    public List<AccountDTO> getAllAccounts() throws IllegalArgumentException {
+        List<AccountDTO> accountDTOs = new ArrayList<AccountDTO>();
+
+        for (Account account : accountService.getAllAccounts()) {
+            accountsDTOs.add(convertToDTO(account));
+        }
+
+        return accountDTOs;
+    }
 }
---
+++ PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/dto/AccountDTO.java
@@ -0,0 +1,14 @@
+package ca.mcgill.ecse321.petadoptionsystem.dto;
+
+import ca.mcgill.ecse321.petadoptionsystem.model.UserRole;
+
+public class AccountDTO {
+
+    public String username;
+    public String email;
+    public UserRole userRole;
+
+    public AccountDTO(String username) {
+        // TODO
+    }
+}
