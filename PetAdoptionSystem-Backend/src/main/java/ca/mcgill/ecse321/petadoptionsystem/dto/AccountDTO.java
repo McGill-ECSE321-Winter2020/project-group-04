700753dd1f8a74fcdc1471c5e7a4ebb9f1d6dd33
garrettkinman <garrettkinman@gmail.com>
2020-02-29 17:58:11 -0500

    was problem with rebase, so committing AccountDTO manually #52

---
+++ PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/dto/AccountDTO.java
@@ -0,0 +1,29 @@
+package ca.mcgill.ecse321.petadoptionsystem.dto;
+
+import ca.mcgill.ecse321.petadoptionsystem.model.UserRole;
+
+public class AccountDTO {
+
+    private String username;
+    private String email;
+    private UserRole userRole;
+
+    @SuppressWarnings("unchecked")
+    public AccountDTO(String username, String email, UserRole userRole) {
+        this.username = username;
+        this.email = email;
+        this.userRole = userRole;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public UserRole getUserRole() {
+        return this.userRole;
+    }
+}
