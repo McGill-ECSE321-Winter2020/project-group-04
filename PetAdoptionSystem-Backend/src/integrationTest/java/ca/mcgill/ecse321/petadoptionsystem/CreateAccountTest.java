736ac23c1727d92cd9c63a854f5de560f7affbd8
tangj0 <jtang186@gmail.com>
2020-03-01 16:28:08 -0500

    set up for integration tests #79

---
+++ PetAdoptionSystem-Backend/src/integrationTest/java/ca/mcgill/ecse321/petadoptionsystem/CreateAccountTest.java
@@ -0,0 +1,18 @@
+package ca.mcgill.ecse321.petadoptionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CreateAccountTest {
+
+    @Test
+    public void createAccountTest(){
+
+    }
+}
