9c1d733061c5697d0ece249e7e8ed737437a7965
Ousmane BARICISSE <ousmane.baricisse@mail.mcgill.ca>
2020-02-29 19:40:12 -0500

    update donationservice + testpetprofile

--- PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/service/DonationService.java
+++ PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/service/DonationService.java
@@ -13,9 +13,15 @@ import org.springframework.stereotype.Service;
 import ca.mcgill.ecse321.petadoptionsystem.dao.AccountRepository;
 import ca.mcgill.ecse321.petadoptionsystem.dao.DonationRepository;
 import ca.mcgill.ecse321.petadoptionsystem.dao.RegularUserRepository;
+import ca.mcgill.ecse321.petadoptionsystem.exception.AmountInvalidException;
+import ca.mcgill.ecse321.petadoptionsystem.exception.NullUsernameException;
 import ca.mcgill.ecse321.petadoptionsystem.model.Account;
 import ca.mcgill.ecse321.petadoptionsystem.model.Donation;
 import ca.mcgill.ecse321.petadoptionsystem.model.RegularUser;
+/**
+ * @author Ousmane Baricisse
+ * Service class for Donation
+ */
 
 @Service
 public class DonationService {
@@ -28,9 +34,22 @@ public class DonationService {
     @Autowired
     private RegularUserRepository regUserRepo;
 
+    /**
+     * 
+     * @param amount
+     * @param date
+     * @param time
+     * @param username
+     * @return
+     */
     @Transactional
     public Donation createDonation(float amount, Date date, Time time, String username){
-        //if(amount ==0 || date==null || time==null || user || == null) throw new 
+        
+        if(amount ==0) throw new AmountInvalidException("Please donate an amount greater than 0. Thank you for your Donation");
+        if(date==null) throw new NullPointerException("Date is currently null. Plense enter correct date value");
+        if(time==null) throw new NullPointerException("Time is currently null. Plense enter correct time value");
+        if(username == null || username.trim()=="") throw new NullUsernameException("The username is null or empty. Please input a username of length >= 1");
+        
         Donation donation = new Donation();
         donation.setAmount(amount);
         donation.setDate(date);
@@ -50,6 +69,7 @@ public class DonationService {
 
     @Transactional
     public List<Donation> getDonationsByUsername(String username){
+        if(username == null || username.trim()=="") throw new NullUsernameException("The username is null or empty. Please input a username of length >= 1");
         List<Donation> donationsByUser = new ArrayList<>();
         Account act =  actRepo.findAccountByUsername(username);
         RegularUser regUser = regUserRepo.findRegularUserByUser(act);
