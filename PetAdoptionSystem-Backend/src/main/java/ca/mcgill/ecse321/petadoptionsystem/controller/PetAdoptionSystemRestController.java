4d67fe7974b463fd74dee4438bf6862a31197c97
garrettkinman <garrettkinman@gmail.com>
2020-03-01 23:18:34 -0500

    javadoc

--- PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/controller/PetAdoptionSystemRestController.java
+++ PetAdoptionSystem-Backend/src/main/java/ca/mcgill/ecse321/petadoptionsystem/controller/PetAdoptionSystemRestController.java
@@ -7,6 +7,9 @@ import ca.mcgill.ecse321.petadoptionsystem.dto.PetAdoptionSystemDto;
 import ca.mcgill.ecse321.petadoptionsystem.model.PetAdoptionSystem;
 import ca.mcgill.ecse321.petadoptionsystem.service.PetAdoptionSystemService;
 
+/**
+ * @author Edem Nuviadenu
+ */
 @CrossOrigin(origins = "*")
 @RestController
 public class PetAdoptionSystemRestController {
@@ -14,18 +17,33 @@ public class PetAdoptionSystemRestController {
     @Autowired
     private PetAdoptionSystemService service;
 
+    /**
+     * 
+     * @return DTO of the created PetAdoptionSystem.
+     * @throws IllegalArgumentException
+     */
     @PostMapping(value = {"/createSystem", "/createSystem"})
     public PetAdoptionSystemDto createPetAdoptionSystem()throws IllegalArgumentException{
          PetAdoptionSystem pas = service.createPetAdoptionSystem();
          return convertToDto(pas);
     }
 
+    /**
+     * 
+     * @return The DTO of the existing PetAdoptionSystem.
+     * @throws IllegalArgumentException
+     */
     @GetMapping(value = {"/getSystem", "/getSystem/"})
     public PetAdoptionSystemDto getPetAdoptionSystem() throws IllegalArgumentException {
         PetAdoptionSystem pas = service.getPetAdoptionSystem();
         return convertToDto(pas);
     }
 
+    /**
+     * 
+     * @param pas The given PetAdoptionSystem.
+     * @return The DTO of the given PetAdoptionSystem.
+     */
     private PetAdoptionSystemDto convertToDto(PetAdoptionSystem pas) {
         return new PetAdoptionSystemDto(pas.getId());
     }
