43a4e7eaa1db88907b68bd9ac75dc42bf6d3b1af
Edem Nuviadenu <edemnuviadenu@gmail.com>
2020-03-02 20:50:40 -0500

    added some extra integration test

--- PetAdoptionSystem-Backend/src/integrationTest/java/ca/mcgill/ecse321/petadoptionsystem/PetSystemIntegrationTests.java
+++ PetAdoptionSystem-Backend/src/integrationTest/java/ca/mcgill/ecse321/petadoptionsystem/PetSystemIntegrationTests.java
@@ -1,15 +1,21 @@
 package ca.mcgill.ecse321.petadoptionsystem;
 
+import ca.mcgill.ecse321.petadoptionsystem.model.PetProfile;
 import org.junit.After;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONArray;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -18,10 +24,13 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.sql.Date;
 import java.sql.Time;
-
+import java.util.ArrayList;
+import java.util.List;
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class PetSystemIntegrationTests {
-    //private final String APP_URL = "http://localhost:8080";
     private final String APP_URL = "http://petadoptionsystem-backend-04-n.herokuapp.com";
     private JSONObject response;
     private final Integer systemID = 4;
@@ -31,6 +40,7 @@ public class PetSystemIntegrationTests {
     private final String restEmailRegularUser= "ecse321testregularuser@protonmail.com";
     private final String restPasswordAdmin = "userpasswordAdmin";
     private final String restPasswordRegularUser = "userpasswordRegularUser";
+    private final String petName = "Gucci";
 
     /*
      * TESTING
@@ -49,6 +59,13 @@ public class PetSystemIntegrationTests {
 
     @Test
     public void testGetPetAdoptionSystem() {
+//        try {
+//            response = send("GET", APP_URL, "/getSystem", null);
+//            System.out.println("Received: " + response.toString());
+//            assertEquals(systemID, response.getString("name"));
+//        } catch (JSONException e) {
+//            fail();
+//        }
 
     }
     @Test
@@ -72,32 +89,78 @@ public class PetSystemIntegrationTests {
     public void testCreateRegularUserAccount() {
 
     }
+    /**
+     * @Author eknuviad
+     */
     @Test
     public void testGetRegularUserbyUser() {
-
+        try {
+            response = send("GET", APP_URL, "/regularuser/" + restUserNameRegular, null);
+            System.out.println("Received: " + response.toString());
+            assertEquals(restUserNameRegular, response.getString("username"));
+        } catch (JSONException e) {
+            fail();
+        }
     }
     @Test
     public void testGetAllPetProfiles() {
-
+        List<PetProfile> profiles = new ArrayList<>();
+        try {
+            response = send("GET", APP_URL, "/petprofiles", null);
+            System.out.println("Received: " + response.toString());
+            assertEquals(petName, response.getString("petName"));
+        } catch (JSONException e) {
+            fail();
+        }
     }
     @Test
     public void testCreateApplication() {
+        try {
+            response = send("POST", APP_URL, "/apply",
+                    "username=" + restUserNameAdmin + "&passwordHash=" + restPasswordAdmin + "&email=" + restEmailAdmin);
+            System.out.println("Received: " + response.toString());
+            assertEquals(restUserNameAdmin, response.getString("username"));
+            assertEquals(restPasswordAdmin, response.getString("passwordHash"));
+            assertEquals(restEmailAdmin, response.getString("email"));
+        } catch (JSONException e) {
+            fail();
+        }
+        //
 
     }
     @Test
     public void testGetApplication() {
-
+     try {
+			String applicationId = send("POST", APP_URL, "/applications/",
+					"username=" + restUserNameRegular + "&email=" + restEmailRegularUser)
+							.getString("applicationId");
+			JSONArray applications = sendArray("GET", APP_URL, "/applications/" + applicationId, "");
+			for (int i = 0; i < applications.length(); i++) {
+				JSONObject o = applications.getJSONObject(i);
+				assertEquals(restUserNameRegular, o.getString("applicant"));
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+			fail();
+		}
     }
     @Test
     public void testUpdateApplication() {
-
+        try {
+			String applicationId = send("POST", APP_URL, "/apply",
+					"username=" + restUserNameRegular + "&email=" + restEmailRegularUser)
+							.getString("applicationId");
+			JSONArray applications = sendArray("GET", APP_URL, "/updateApplication/" + applicationId, "");
+			for (int i = 0; i < applications.length(); i++) {
+				JSONObject o = applications.getJSONObject(i);
+				assertEquals(true, o.getString("confirm"));
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+			fail();
+		}
     }
 
-    @After
-    public void zClearDb() {
-        send("POST", APP_URL, "/flushdb", null);
-        assertTrue(true);
-    }
 
     // METHODS FOR REST
 
