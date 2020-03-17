97685ff72d49ab820ca78cf4308d3fd3396cd5ba
tangj0 <jtang186@gmail.com>
2020-03-17 13:50:51 -0400

    link login to welcome #94

--- PetAdoptionSystem-Frontend/src/router/index.js
+++ PetAdoptionSystem-Frontend/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Welcome from '@/components/Welcome'
-import Hello from '@/components/Hello'
 import Login from '@/components/Login'
 
 Vue.use(Router)
@@ -12,11 +11,6 @@ export default new Router({
       path: '/',
       name: 'Welcome',
       component: Welcome
-    },
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
     },
     {
       path: '/login',
