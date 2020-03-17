4537c77108c1c859e37b34eda57632115c77a1ad
tangj0 <jtang186@gmail.com>
2020-03-17 13:50:51 -0400

    link login to welcome #94

--- PetAdoptionSystem-Frontend/src/router/index.js
+++ PetAdoptionSystem-Frontend/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Welcome from '@/components/Welcome'
-import Hello from '@/components/Hello'
 import Login from '@/components/Login'
 import Donation from '@/components/Donation'
 
